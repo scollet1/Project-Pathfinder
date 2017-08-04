e2892990890edd274e7ee302f4ae075807ad3098
Sam Collet <scollet@student.42.us.org>
2017-08-04 15:22:43 -0700

    Update pseudo_code.py

--- python/pseudo_code.py
+++ python/pseudo_code.py
@@ -6,7 +6,6 @@ import dependencies such as numpy, math, $LIBRAIRIES
 
 EXPLORATION = "e-greedy"
 EPISODES = 20000 # large enough to converge, small enough that it doesn't take years
-define loss value = some num
 LEARNING_RATE = some num between [0.0000, 0.0004] # we can play around with this though
 MEMORY = large value # where we're going to store our long-short-term memory 
 BATCH_SIZE = 64 # how many nodes do we have??? [64, ~ 100] is a good range
