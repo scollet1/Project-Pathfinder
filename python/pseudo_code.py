c56c00f1ea40a942ce8f971e1f3016b61c348218
Sam Collet <scollet@student.42.us.org>
2017-08-04 13:27:34 -0700

    Update pseudo_code.py

--- python/pseudo_code.py
+++ python/pseudo_code.py
@@ -2,18 +2,21 @@
 
 import dependencies such as numpy, math, $LIBRAIRIES
 
+# ~GLOBALS~ #
+
 define loss value = some num
-learning rate = some num between [0.0000, 0.0004] #we can play around with this though
-memory = large value
-batch size = how many nodes do we have??? [64, ~ 100] is a good range
-gamma = this modifies the reward by diminishing returns over time [0, 1]
-lambda = rate of decay
-EPSILON_MAX = 1 #this is for a greedy epsilon problem to determine what actions we search for [0, 1]
+LEARNING_RATE = some num between [0.0000, 0.0004] # we can play around with this though
+MEMORY = large value # where we're going to store our long-short-term memory 
+BATCH_SIZE = 64 # how many nodes do we have??? [64, ~ 100] is a good range
+GAMMA = 0.004 # this discounts the reward by diminishing returns over time [0, 1]
+LAMBDA = rate of decay
+EPSILON_MAX = 0.999 #this is for a greedy epsilon problem to determine what actions we search for [0, 1]
+EPSILON_MIN = 0.001
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 class Brain:
-  def init(some params)
+  def __init__(some params)
     initialize
    
   def train(some params)
@@ -34,7 +37,7 @@ class Brain:
 class Robot:
   epsilon = EPSILON_MAX
   
-  def init(params)
+  def __init__(params)
     initialize
   
   def act() #roll the dice for a move
@@ -57,7 +60,7 @@ class Robot:
   
 class Domain: #our simulated world
   
-  def init(params)
+  def __init__(params)
     init map
     
   def run(robot, agents)
@@ -74,7 +77,7 @@ class Domain: #our simulated world
     
 class Agents:
   
-  def init(params)
+  def __init__(params)
    
   def pathfinding(params)
   
